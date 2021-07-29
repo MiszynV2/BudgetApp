80feb07d9bc6b22266d4aa8cb57826b390eed28d
Miszyn <wmarciniszyn11@gmail.com>
2021-07-29 18:29:41 +0200

    added event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import { useState} from 'react'
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
 
-  console.log(props.date);
+  const clickHandler = () =>{
+    setTitle(Math.random)}
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-
+        <button onClick={clickHandler}>Change Title</button>
       </div>
-    </div>
+    </Card>
   );
 }
 
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,18 @@
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm';
+const NewExpense = (props) => {
+    const onSaveExpenseData = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        props.onAddEspense(expenseData)
+    }
+    return(
+        <div className="new-expense">
+           <ExpenseForm onSaveExpenseData = {onSaveExpenseData}></ExpenseForm>
+        </div>
+    )
+};
+
+export default NewExpense
