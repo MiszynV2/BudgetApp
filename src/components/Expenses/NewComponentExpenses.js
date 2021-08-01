79755f20556771ca4137b5c40636bd04733978e2
Miszyn <wmarciniszyn11@gmail.com>
2021-08-01 10:37:18 +0200

    Finally version of this project

--- src/components/Expenses/NewComponentExpenses.js
+++ src/components/Expenses/NewComponentExpenses.js
@@ -1,33 +1,33 @@
-//import ExpenseDate from "./ExpenseDate";
-import ExpenseItem from "./ExpenseItem";
+
 import "./NewComponentExpenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpenseChart";
 
-function NewComponentExpenses(props){
-    return(
-    <Card  className='expenses'>
-        <ExpenseItem 
-      title={props.expenses[0].title} 
-      amount={props.expenses[0].amount} 
-      date={props.expenses[0].date}>
-      </ExpenseItem >
-      <ExpenseItem
-            title={props.expenses[1].title} 
-            amount={props.expenses[1].amount} 
-            date={props.expenses[1].date}>
-      </ExpenseItem>
-      <ExpenseItem
-          title={props.expenses[2].title} 
-          amount={props.expenses[2].amount} 
-          date={props.expenses[2].date}>
-      </ExpenseItem>
-      <ExpenseItem
-            title={props.expenses[3].title} 
-            amount={props.expenses[3].amount}    
-            date={props.expenses[3].date}>
-      </ExpenseItem>
-    </Card>
-    )
+function NewComponentExpenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
+  const filteredExpenses = props.expenses.filter(el => {
+      return el.date.getFullYear().toString() === filteredYear
+  })
+
+  
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangedHandler}
+        ></ExpensesFilter>
+        <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
+        <ExpensesList items = {filteredExpenses}></ExpensesList>
+      </Card>
+    </div>
+  );
 }
-export default NewComponentExpenses
+export default NewComponentExpenses;
